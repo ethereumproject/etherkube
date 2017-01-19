b952f6c38eef3650fda37acd3a27c490c7691739
Igor Artamonov <igor@artamonov.ru>
2017-01-19 21:28:45 +0600

    display height timestamp

--- dashboard-ui/src/components/nodes.js
+++ dashboard-ui/src/components/nodes.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux'
 import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table'
+import Moment from 'react-moment';
 import log from 'loglevel';
 
 const Render = ({nodes}) => {
@@ -14,16 +15,22 @@ const Render = ({nodes}) => {
                 <TableHeaderColumn style={shortStyle}>ID</TableHeaderColumn>
                 <TableHeaderColumn style={shortStyle}>Since</TableHeaderColumn>
                 <TableHeaderColumn style={shortStyle}>Height</TableHeaderColumn>
+                <TableHeaderColumn style={shortStyle}>Timestamp</TableHeaderColumn>
                 <TableHeaderColumn style={wideStyle}>Hash</TableHeaderColumn>
             </TableRow>
         </TableHeader>
         <TableBody displayRowCheckbox={false}>
             {nodes.map( (node) => {
+                const haveData = typeof (node.block) === 'object';
+                const ts = haveData ? new Date(parseInt(node.block.timestamp, 16) * 1000) : null;
             return (
                 <TableRow key={node.service.metadata.uid} selectable={false}>
                     <TableRowColumn style={shortStyle}>{node.service.metadata.name}</TableRowColumn>
                     <TableRowColumn style={shortStyle}>{node.service.metadata.creationTimestamp}</TableRowColumn>
                     <TableRowColumn style={shortStyle}>{node.height}</TableRowColumn>
+                    <TableRowColumn style={shortStyle}>
+                        <Moment fromNow>{ts}</Moment>
+                    </TableRowColumn>
                     <TableRowColumn style={wideStyle}>{typeof(node.block) === 'object' ? node.block.hash : ''}</TableRowColumn>
                 </TableRow>
             );
